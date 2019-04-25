aa7846f2e5f31463ae52c592ac478a85dbe0da68
Matthew Morgan <mmorgan55@cnm.edu>
2019-04-25 15:17:26 -0600

    Started basic layout for game area.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,55 @@ void main() => runApp(DivideGame());
 class DivideGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      title: "MathDivide!",
+      home: Scaffold(
+        body: App(),
+      ),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        height: 350.0,
+        width: 350.0,
+        color: Colors.teal,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    color: Colors.black,
+                    height: 75.0,
+                    width: 75.0,
+                  ),
+                  Container(
+                    color: Colors.black,
+                    height: 75.0,
+                    width: 75.0,
+                  ),
+                  Container(
+                    color: Colors.black,
+                    height: 75.0,
+                    width: 75.0,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
